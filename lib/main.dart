2f032f30c70b3d57e7e909ab50dc97689eacf4e1
Jeremiah Jacob <jaykon4me@gmail.com>
2021-12-07 11:10:25 +0100

    initial comit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/components/custom_list_tile.dart';
+import 'package:news_app/model/article_model.dart';
+import 'package:news_app/services/api_service.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ApiService client = ApiService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text("News App", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.black),
+      body: FutureBuilder(
+        future: client.getArticle(),
+        builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+          if (snapshot.hasData) {
+            List<Article> articles = snapshot.data;
+            return ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: (context, index) =>
+                  customListTile(articles[index], context),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
